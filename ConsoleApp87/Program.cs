b91780ca360d8148237188617a53526132cadd85
topmbr <centerm9911@gmail.com>
2023-11-26 14:16:50 +0200

    Добавьте файлы проекта.

---
+++ ConsoleApp87/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace ConsoleApp87
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Task<int> task = Task.Run(() =>
+            {
+                return CountPrimesInRange(0, 1000);
+            });
+
+            // Очікуємо на завершення завдання і отримуємо результат
+            int primeCount = task.Result;
+
+            Console.WriteLine($"Кількість простих чисел від 0 до 1000: {primeCount}");
+            Console.WriteLine("Головний потік завершено.");
+            Console.ReadLine();
+        }
+        static int CountPrimesInRange(int start, int end)
+        {
+            int count = 0;
+            for (int i = start; i <= end; i++)
+            {
+                if (IsPrime(i))
+                {
+                    Console.Write($"{i} ");
+                    count++;
+                }
+            }
+            return count;
+        }
+
+        static bool IsPrime(int number)
+        {
+            if (number < 2)
+                return false;
+
+            for (int i = 2; i <= Math.Sqrt(number); i++)
+            {
+                if (number % i == 0)
+                    return false;
+            }
+
+            return true;
+        }
+    }
+}
